a0009f29723c8b0d9f104d4e8a81fd5a22e8590f
pjnalls <42269808+pjnalls@users.noreply.github.com>
2023-03-10 06:56:20 -0600

    Add a countdown timer until π 2023.

--- static/app.js
+++ static/app.js
@@ -1,8 +1,35 @@
 (() => {
+  const pi = new Date("March 14, 2023 00:00:00");
+
+  let = time = new Date();
+
+  const decrementTimeByASecond = () => {
+    time = new Date(time.getTime() + 1000);
+
+    const diff = pi.getTime() - time.getTime();
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+
+    if (days === 0) document.getElementById("in").innerText = "";
+
+    document.getElementById("time").innerText = `${
+      days === 0 ? "Tomorrow" : `${days} ${days > 1 ? "days" : "day"}`
+    }, ${Math.ceil((diff / (1000 * 60 * 60)) % 24)
+      .toString()
+      .padStart(2, "0")}:${Math.floor((diff / (1000 * 60)) % 60)
+      .toString()
+      .padStart(2, "0")}:${Math.floor((diff / 1000) % 60)
+      .toString()
+      .padStart(2, "0")}`;
+  };
+
+  decrementTimeByASecond();
+
   if (
     window.matchMedia &&
     window.matchMedia("(prefers-color-scheme: dark)").matches
   ) {
     document.getElementById("body").classList.value = "dark";
   }
+
+  setInterval(() => decrementTimeByASecond(), 1000);
 })();
